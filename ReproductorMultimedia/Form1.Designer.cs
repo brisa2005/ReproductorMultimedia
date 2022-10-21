a06796b45f577d0391d0676191e02f3a29cac485
Alumno <Alumno@PC-38>
2022-10-21 09:15:27 -0500

    Agregar archivos de proyecto.

---
+++ ReproductorMultimedia/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ReproductorMultimedia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mpMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.lbListaArchivos = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mpMediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mpMediaPlayer
+            // 
+            this.mpMediaPlayer.Enabled = true;
+            this.mpMediaPlayer.Location = new System.Drawing.Point(11, 24);
+            this.mpMediaPlayer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mpMediaPlayer.Name = "mpMediaPlayer";
+            this.mpMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mpMediaPlayer.OcxState")));
+            this.mpMediaPlayer.Size = new System.Drawing.Size(339, 408);
+            this.mpMediaPlayer.TabIndex = 0;
+            this.mpMediaPlayer.Enter += new System.EventHandler(this.mpMediaPlayer_Enter);
+            // 
+            // lbListaArchivos
+            // 
+            this.lbListaArchivos.FormattingEnabled = true;
+            this.lbListaArchivos.Location = new System.Drawing.Point(367, 24);
+            this.lbListaArchivos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lbListaArchivos.Name = "lbListaArchivos";
+            this.lbListaArchivos.Size = new System.Drawing.Size(227, 355);
+            this.lbListaArchivos.TabIndex = 1;
+            this.lbListaArchivos.SelectedIndexChanged += new System.EventHandler(this.lbListaArchivos_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(484, 400);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Abrir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 442);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbListaArchivos);
+            this.Controls.Add(this.mpMediaPlayer);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reproductor Multimedia";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mpMediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer mpMediaPlayer;
+        private System.Windows.Forms.ListBox lbListaArchivos;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
